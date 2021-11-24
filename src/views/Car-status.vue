dbaef14067dcbfd498da7dc59c0c569245cb1f08
car-home <422451702@qq.com>
2021-11-24 23:53:59 +0800

    network

--- src/views/Car-status.vue
+++ src/views/Car-status.vue
@@ -3,15 +3,15 @@
 		<admin-slot>
 			<template #title>社区车位情况</template>
 			<template #content>
-				<div v-for="(item,index) in app.parkList" :key="index">
-					<div>{{index+1}}号停车位</div>
+				<div v-for="(item,index) in parking" :key="index">
+					<div>{{item.parklot}}号停车位</div>
 					<el-card :body-style="{ padding: '5px',display:'flex','flex-direction':'row' }">
-						<div class="card" v-if="item.status">
+						<div class="card" v-if="item.hasCar">
 							<img class="card-img" src="@/assets/image/car.jpg" alt="" />
 							<div class="card-text">
-								<p>{{item.car}}</p>
-								<p>时长：{{item.time}}分钟</p>
-								<p>预计费用：{{item.price}}元</p>
+								<p>{{item.carNum}}</p>
+								<p>时长：{{parseInt(-(parseInt(item.startTime)-parkTime)/60000)}}分钟</p>
+								<p>预计费用：{{parseInt(-(parseInt(item.startTime)-parkTime)/60000)-30 > 0 ? parseInt(-(parseInt(item.startTime)-parkTime)/1000000):0}}元</p>
 							</div>
 						</div>
 						<div class="card-else" v-else>
@@ -27,49 +27,41 @@
 <script>
 import adminSlot from "@/components/admin-content/admin-content.vue";
 import { getParking } from "@/network/car"
-import { onBeforeMount, reactive } from "vue-demi";
+import { computed, onBeforeMount, ref } from "vue-demi";
 export default {
 	components: {
 		adminSlot,
 	},
 	setup() {
+		// 停车场状态对象
+		const parking = ref({})
+
+		const parkTime = computed(()=>{
+			var time = Date.now()
+			if(parking.value){
+				console.log(-(parseInt(parking.value[3].startTime)-time)/60000);
+				console.log(time);
+				return time
+			}
+		})
+
+		// 生命周期
 		onBeforeMount(async ()=>{
 			//请求停车场状态
 			try{
-				let parking = await getParking();
+				const {data} = await getParking();
+				parking.value = data.data
 				console.log(parking);
 			}catch(e){
 				console.log(e);
 			}
 		})
-		const app = reactive({
-			parkList: [
-				{
-					car: "粤A66666",
-					time: "26",
-					price: "5",
-					status:true
-				},
-				{
-					car: "粤A66668",
-					time: "59",
-					price: "10",
-					status:true
-				},
-				{
-					status:false
-				},
-				{
-					car: "粤A66668",
-					time: "59",
-					price: "10",
-					status:true
-				},
-			],
-		});
-		return { app };
+		
+		return { parking,parkTime };
 	},
+
 };
+
 </script>
 
 <style scoped>
